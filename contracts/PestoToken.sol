4dddae934d8c55d63f6fdc35dc9d26251ce63c9a
Shahbaz Khan <shahbaz@Shahbazs-MacBook-Pro.local>
2023-06-10 17:07:18 +0530

    add test cases

--- contracts/PestoToken.sol
+++ contracts/PestoToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PestoToken is ERC20 {
-    constructor() ERC20("Pesto Token", "PESTO") {
+    constructor() ERC20("PestoToken", "PESTO") {
         _mint(msg.sender, 1000000 * 10**18);
     }
 }
